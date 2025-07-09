3e1ae57597984f5f463b3f1924fc2fd7731680ce
NAYEONG OH <nayeong.oh@mataedu.com>
2025-07-09 23:24:00 +0900

    feat: qrCode 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body>
         <StyledComponentsRegistry>
           <GlobalStyle />
