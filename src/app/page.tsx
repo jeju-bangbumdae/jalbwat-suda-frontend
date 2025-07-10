891e606f5dad2ec51d4b5a72db548eb91016db07
jangsehee <julia8024@naver.com>
2025-07-10 14:24:27 +0900

    chore: config 파일에 api url 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
 'use client';
+import { BottomNav } from '@/components/navigation/BottomNav';
 import { Button, Text } from '@vapor-ui/core';
+import axios from 'axios';
+import { useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
 
 export default function Home() {
-  return (
+
+  const searchParams = useSearchParams();
+  // const qrcode = searchParams.get('qrcode'); // 문자열로 반환됨 (예: 'true', 'false', null)
+  // const [input, setInput] = useState("");
+
+  const getData = async () => {
+      try {
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api`)
+        console.log(res)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  
+    useEffect(() => {
+      getData();
+    }, [])
+
+  return (<>
     <main>
       <Button>너 됨?</Button>
       <ol>
@@ -14,5 +37,7 @@ export default function Home() {
         <Text style={{color: "var(--color-primary-orange)"}}>테스트 문구! - vapor 텍스트</Text>
       </ol>
     </main>
+    <BottomNav />
+  </>
   );
 }
