787df998018e9a17b96f440f0bccc1803818e19e
NAYEONG OH <nayeong.oh@mataedu.com>
2025-07-10 19:17:04 +0900

    feat: 홈화면 컴포넌트 이동

--- src/app/(main)/home/HomePage.tsx
+++ src/app/(main)/home/HomePage.tsx
@@ -1,25 +1,28 @@
-"use client";
-import axios from "axios";
-import { useEffect } from "react";
-
+'use client';
+import { MainSwiper } from '@/components/main/MainSwiper';
+import { BottomNav } from '@/components/navigation/BottomNav';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 export default function HomePage({ baseURL }) {
+  console.log('!!', baseURL);
 
-	console.log("!!", baseURL)
+  const getData = async () => {
+    try {
+      const res = await axios.get(`${baseURL}/health`);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const getData = async () => {
-		try {
-			const res = await axios.get(`${baseURL}/health`)
-			console.log(res)
-		} catch (error) {
-			console.log(error)
-		}
-	}
-
-	useEffect(() => {
-		getData();
-	}, [])
-	return (
-		<div>text</div>
-	)
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div>
+      <MainSwiper />
+      <BottomNav />
+    </div>
+  );
 }
